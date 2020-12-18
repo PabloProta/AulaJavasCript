04d5bfe4a5a2abc17047173dec2f3cc503470888
Pablo Prota <antoniniprota@gmail.com>
2020-12-18 19:18:15 -0300

    adicionando modelo para exercicios

---
+++ modelo/assets/js/main.js
@@ -0,0 +1,11 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+ 
+
+
+  btnTarefa.addEventListener('click',function(e){
+
+    console.log(inputTarefa.value);
+
+  })
